caf7fa8b488bc36145a672bf7211433432332653
elias <eliascoelho385@gmail.com>
2023-11-07 09:50:45 -0300

    Chamado de uma função

---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
+
+/*	No terminal, vá para este caminho: "/fundamentos/funcoes"
+	Logo após, execute o comando no terminal: "go run *.go"	*/
